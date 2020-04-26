643e2127a1227825aa490b73f956f90bb65cfc6e
Kalva014 <kalva014@ucr.edu>
2020-04-25 23:35:33 -0700

    Failed part 3

--- source/main.c
+++ source/main.c
@@ -1,8 +1,8 @@
 /*	Author: kennethalvarez
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #5  Exercise #2
- *	Exercise Description: Pressing PA1 decrements PORTC (stopping at 0). If both buttons are depressed (even if not initially simultaneously), PORTC resets to 0.
+ *	Assignment: Lab #5  Exercise #3
+ *	Exercise Description: Create your own festive lights display 
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -13,8 +13,8 @@
 #endif
 
 
-enum States {Start, Init, ButtonPressed1, OnRelease1, ButtonPressed2, OnRelease2, Reset} state;
-
+enum States {Start, Init, Light13, Release13, Light24, Release24, Light35, Release35, LightAll} state;
+unsigned char TMPB = 0x00;
 
 void Tick() {
 	switch(state) { //transitions
@@ -23,91 +23,115 @@ void Tick() {
 			break;
 		case Init:
 			if((~PINA & 0x01) == 0x01) {
-				state = ButtonPressed1;
-			}
-			else if((~PINA & 0x02) == 0x02) {
-				state = ButtonPressed2;
+				state = Light13;
 			}
 			else {
 				state = Init;
 			}
-				
-			if((~PINA & 0x03) == 0x03) { //RESET STATEMENT MIGHT BE WRONG
-				state = Reset;
-			}
 			break;
-		case ButtonPressed1:
-			state = OnRelease1;
-
-			if((~PINA & 0x03) == 0x03) { //RESET STATEMENT MIGHT BE WRONG
-                                state = Reset;
-                        }
-			break;
-		case OnRelease1:
+		case Light13:
 			if((~PINA & 0x01) == 0x01) {
-				state = OnRelease1;
-			}
-			else {
-				state = Init;
-			}
-			break;
-		case ButtonPressed2:
-			state = OnRelease2;
-
-			if((~PINA & 0x03) == 0x03) { //RESET STATEMENT MIGHT BE WRONG
-       				state = Reset;
+                                state = Light13;
                         }
+			else {
+				state = Release13;
+			}
 			break;
-		case OnRelease2:
-			if((~PINA & 0x02) == 0x02) { //If button is held down then stay in release
-				state = OnRelease2;
+		case Release13:
+			if((~PINA & 0x01) != 0x01) {
+				state = Light24;
+			}
+			else {
+				state = Release13;
+			}
+			break;
+		case Light24:
+			if((~PINA & 0x01) == 0x01) {
+				state = Light24;
+			}
+			else {
+				state = Release24;
+			}
+			break;
+		case Release24:
+                        if((~PINA & 0x01) != 0x01) {
+                                state = Light35;
+                        }
+                        else {
+                                state = Release24;
+                        }
+                        break;
+		case Light35:
+			if((~PINA & 0x01) == 0x01) {
+       				state = Light35;
+                        }
+			else {
+				state = Release35;
+			}
+			break;
+		case Release35:
+                        if((~PINA & 0x01) != 0x01) {
+                                state = LightAll;
+                        }
+                        else {
+                                state = Release35;
+                        }
+                        break;
+		case LightAll:
+			if((PINA & 0x01) == 0x01) { //If button is held down then stay in release
+				state = LightAll;
 			}
 			else {
 				state = Init;
 			}
-			break;
-		case Reset:
-			state = Init;
 			break;
 		default:
 			break; 
 	}
+
 	switch(state) { //actions
 		case Start:
+                        TMPB = 0x00;
+                        break;
+                case Init:
+			TMPB = 0x00;
+                        break;
+                case Light13:
+                        TMPB = 0x05; //0000 0101
+                        break;
+                case Release13:	
 			break;
-		case Init:
+		case Light24:
+			TMPB = 0x0A; //0000 1010
+                        break;
+		case Release24:
 			break;
-		case ButtonPressed1:
-			if(PINC < 9) {
-				PORTC = PINC +  0x01;
-			}
+                case Light35:
+			TMPB = 0x14; //0001 0100
+                        break;
+		case Release35:
 			break;
-		case OnRelease1:
-			break;
-		case ButtonPressed2:
-			if(PINC > 0) {
-				PORTC = PINC - 0x01;
-			}
-			break;
-		case OnRelease2:
-			break;
-		case Reset:
-			PORTC = 0x00;
-			break;
-		default:
-			break;
+                case LightAll:
+			TMPB = 0x1F; //0001 1111
+                        break;
+                default:
+                        break; 
 	}
+
+	PORTB = TMPB;
 }
 
 
 int main(void) {
     /* Insert DDR and PORT initializations */
         DDRA = 0x00; PORTA = 0xFF;
-        DDRC = 0xFF; PORTC = 0x00;
+        DDRB = 0xFF; PORTB = 0x00;
 	state = Start; //Inital state		
-
+	//unsigned char TMPB = 0x00;
+	
     /* Insert your solution below */
     while (1) {
+	TMPB = 0x00;
 	Tick();
     }
        return 1;
