9e19289c96f187c281aed57a3cc4ef3a5f2a2c53
Kalva014 <kalva014@ucr.edu>
2020-04-25 20:54:13 -0700

    Finished part 1

--- turnin/kalva014_lab5_part1.c
+++ turnin/kalva014_lab5_part1.c
@@ -13,37 +13,40 @@
 #endif
 
 int main(void) {
-  int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRC = 0xFF; PORTC = 0x00;
-	
+	unsigned char TMPC = 0x00;
+		
     /* Insert your solution below */
     while (1) {
 	
-	button = ~PINA; // Since button when pushed is 0 flip every bit
+	//button = ~PINA; // Since button when pushed is 0 flip every bit
 
-	if (button == 0) {
-		PORTC = 0x40;
+	if(((~PINA & 0x0F) > 0) && (~PINA & 0x0F) <= 2) {
+		TMPC = 0x20; //0010 0000
 	}
-	else if(button <= 2) {
-		PORTC = 0x60; //0110 0000
+	else if(((~PINA & 0x0F) == 3) || ((~PINA & 0x0F) == 4)) {
+		TMPC = 0x30; //0011 0000
 	}
-	else if((button <= 4) && (button > 2)) {
-		PORTC = 0x70; //0111 0000
+	else if(((~PINA & 0x0F) == 6) || ((~PINA & 0x0F) == 5)) {
+		TMPC = 0x38; //0011 1000
 	}
-	else if((button <= 6) && (button >= 5)) {
-		PORTC = 0x38; //0011 1000
+	else if(((~PINA & 0x0F) == 9) || ((~PINA & 0x0F) == 8) || ((~PINA & 0x0F) == 7)) {
+		TMPC = 0x3C; //0011 1100
 	}
-	else if((button <= 9) && (button >= 7)) {
-		PORTC = 0x3C; //0011 1100
+	else if(((~PINA & 0x0F) == 12) || ((~PINA & 0x0F) == 11) || ((~PINA & 0x0F) == 10)) {
+		TMPC = 0x3E; //0011 1110
 	}
-	else if((button <= 12) && (button >= 10)) {
-		PORTC = 0x3E; //0011 1110
+	else if(((~PINA & 0x0F) == 15) || ((~PINA & 0x0F) == 14) || ((~PINA & 0x0F) == 13)) {
+		TMPC = 0x3F; //0011 1111
 	}
-	else if((button <= 15) && (button >= 13)) {
-		PORTC = 0x3F ; //0011 1111
+
+	if((~PINA & 0x0F) <= 4) {
+		TMPC = TMPC | 0x40 ; //0100 0000
 	}
+	
+	PORTC = TMPC;
     }
     return 1;
 }
